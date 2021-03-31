2bad5d4125035bb8e3cb4841d5a38c27aab22296
Rom4io <salyahov@gmail.com>
2021-03-31 16:41:44 +0300

    add

--- src/Calc.java
+++ src/Calc.java
@@ -5,12 +5,13 @@ public class Calc {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Введите выражение: ");
-        String str1 = scanner.next();
-        String str2 = str1.replaceAll(" ","");
-        String str = str2.toUpperCase();
+        String str1 = scanner.nextLine();
+        String str2 = str1.toUpperCase();
+        String str = str2.replaceAll("\\s+", "");
         char[] chars = str.toCharArray();
         int symbolIndex = 0;
         char ch = 0;
+        System.out.println(str);
         for (int i = 1; i < chars.length - 1; i++) {
             switch (chars[i]) {
                 case ('+'):
@@ -36,9 +37,9 @@ public class Calc {
             }
         }
         if (Character.isDigit(chars[0]) && Character.isDigit(chars[chars.length-1])){
-            System.out.println("=" + Latin.Calculate(str, symbolIndex,ch));}
+            System.out.print("=" + Latin.Calculate(str, symbolIndex,ch));}
         else if (Character.isLetter(chars[0]) && Character.isLetter(chars[chars.length-1])) {
-            System.out.println(Roma.Calculate(str, symbolIndex, ch));
+            System.out.print(Roma.Calculate(str, symbolIndex, ch));
         } else throw new ArithmeticException("Некорректное выражение");
     }
 }
